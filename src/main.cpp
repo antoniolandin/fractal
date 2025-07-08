8a277b2bcf4767406609edcc6858c4c8f8229c36
antonio <antoniolandin@proton.me>
2025-07-08 10:48:45 +0200

    fix: first row fractal error

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,13 @@
 
 int main(int argc, char const* argv[])
 {
-    const int ANCHO_PANTALLA = 320;
-    const int ALTO_PANTALLA = 180;
+    const int ANCHO_PANTALLA = 1080;
+    const int ALTO_PANTALLA = 720;
+    const int DOWNSCALE = 5;
+    const int ANCHO = ANCHO_PANTALLA / DOWNSCALE;
+    const int ALTO = ALTO_PANTALLA / DOWNSCALE;
     const double C_X = 0;
-    const double C_Y = 0;
+    const double C_Y = 2;
     const double ZOOM = 2;
     const int MAX_X_ORIGINAL = 5;
     const int MIN_X_ORIGINAL = -5;
@@ -24,8 +27,8 @@ int main(int argc, char const* argv[])
     const double MIN_Y = MIN_Y_ORIGINAL / ZOOM + C_Y;
     const short unsigned int MAX_BOTES = 1000;
 
-    std::vector<double> X = linspace_double(MIN_X, MAX_X, ANCHO_PANTALLA);
-    std::vector<double> Y = linspace_double(MIN_Y, MAX_Y, ALTO_PANTALLA);
+    std::vector<double> X = linspace_double(MIN_X, MAX_X, ANCHO);
+    std::vector<double> Y = linspace_double(MIN_Y, MAX_Y, ALTO);
 
     // Rellenamos los arrays de mapeo de colores que más tarde nos ayudaran a transformar un número en un color
     int STEP = 32;
@@ -47,31 +50,36 @@ int main(int argc, char const* argv[])
         }
     }
 
-    // Utilizando multiprocessing, calculamos todos los botes en un vector
+    // Utilizando multiprocessing, calculamos todos los promesas en un vector
     BS::thread_pool_light pool;
-    std::vector<std::future<int>> botes;
+    std::vector<std::future<int>> promesas;
 
-    for (auto y = Y.end(); y != Y.begin(); --y) {
+    int head = 5;
+    int cont = 0;
+    int next = 0;
+
+    for (auto y = Y.rbegin(); y != Y.rend(); ++y) {
         for (auto x = X.begin(); x != X.end(); ++x) {
-            botes.push_back(pool.submit(calcular_botes, *x, *y, MAX_BOTES));
+            promesas.push_back(pool.submit(calcular_botes, *x, *y, MAX_BOTES));
         }
     }
 
-    // Una vez calculado el vector, transformamos ese vector de botes en una imagen
+    // Una vez calculado el vector, transformamos ese vector de promesas en una imagen
     sf::Image image;
-
-    image.create(ANCHO_PANTALLA, ALTO_PANTALLA);
+    image.create(ANCHO, ALTO);
 
     int r, g, b;
     int pos_x = 0;
     int pos_y = 0;
+    int botes = 0;
 
-    for (auto i = botes.begin(); i != botes.end(); ++i) {
-        map_to_color(i->get(), map_r, map_g, map_b, len, &r, &g, &b);
+    for (auto i = promesas.begin(); i != promesas.end(); ++i) {
+        botes = i->get();
+        map_to_color(botes, map_r, map_g, map_b, len, &r, &g, &b);
         image.setPixel(pos_x, pos_y, sf::Color(r, g, b));
 
         pos_x++;
-        if(pos_x >= ANCHO_PANTALLA) {
+        if(pos_x >= ANCHO) {
             pos_x = 0;
             pos_y++;
         }
@@ -86,6 +94,7 @@ int main(int argc, char const* argv[])
     // Create a sprite from the texture
     sf::Sprite sprite;
     sprite.setTexture(texture);
+    sprite.setScale(DOWNSCALE, DOWNSCALE);
 
     sf::RenderWindow window(sf::VideoMode(ANCHO_PANTALLA, ALTO_PANTALLA), "fractal");
 
